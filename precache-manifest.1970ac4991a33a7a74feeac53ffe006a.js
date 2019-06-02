91cbb91be127b2b236707f60fe1295376f70187a
Klaudia Waclawska-Cwiertnia <kaywoo@o2.pl>
2019-06-02 16:18:56 +0200

    Updates

---
+++ precache-manifest.1970ac4991a33a7a74feeac53ffe006a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8189f4f8b1748e2a0796d71bc9a0ca",
+    "url": "/Beers/index.html"
+  },
+  {
+    "revision": "744a9cf53ca47e20a0b1",
+    "url": "/Beers/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "ee239430b6edf0298bef",
+    "url": "/Beers/static/css/main.e73fd0e7.chunk.css"
+  },
+  {
+    "revision": "744a9cf53ca47e20a0b1",
+    "url": "/Beers/static/js/2.17f5b771.chunk.js"
+  },
+  {
+    "revision": "ee239430b6edf0298bef",
+    "url": "/Beers/static/js/main.37b36064.chunk.js"
+  },
+  {
+    "revision": "bd48c338d68c61525f81",
+    "url": "/Beers/static/js/runtime~main.2ca3b019.js"
+  }
+]);
